c2abdcdea061d40280e4a0db8514094d7c1042ce
SuperSniper1018 <fourteendaystime@gmail.com>
2023-03-10 20:58:40 -0700

    Minor Updates
    Some basic Js for the navbar crap

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,3 +1,9 @@
-function scrollToTop() {
-    window.scrollTo();
-}
+document.addEventListener("scroll", function() {
+    var navbar = document.querySelector("nav");
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    if (scrollTop === 0) {
+      navbar.classList.add("transparent");
+    } else {
+      navbar.classList.remove("transparent");
+    }
+  });  
